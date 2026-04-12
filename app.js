e7155c74738a1bddb17083b4f125473ebc4ea5a4
ahmedrazadars <ahmeddars110@gmail.com>
2026-04-12 15:44:32 +0500

    new updt

---
+++ app.js
@@ -0,0 +1,101 @@
+console.log('Ahmed Raza');
+
+
+let country = {
+    Name: 'Pakistan',
+    Provin: ['Sindh', 'Punjab', 'Balochistan', 'Kpk'],
+    area: 996990,
+    isParliamentrySystem: true,
+    areaUnit: 'SQ KL'
+};
+
+console.log(country);
+
+
+
+
+let patient1 ={
+    name: 'Akram Khan',
+    sympotoms: ['Fever', 'Flue', 'Cough'],
+    coronsa: false,
+    isolation: false
+};
+
+let patient2 ={
+    name: 'Aslam Khan',
+    sympotoms: ['Fever', 'Flue', 'Cough'],
+    coronsa: true,
+    isolation: true
+};
+
+let patient3 ={
+    name: 'Munshi Khan',
+    sympotoms: ['Flue', 'Cough'],
+    coronsa: false,
+    isolation: false
+};
+
+let patients = [patient1, patient2, patient3];
+
+console.log(patients);
+
+
+
+
+
+
+
+
+
+
+
+
+let studentName = prompt('Student Name');
+let rollNumber = prompt('Your Roll Number?');
+let subject1 = prompt('Write Your Subject name');
+let subject2 = prompt('Write Your Subject name');
+
+let Student ={
+    name: studentName,
+    rollNumber: rollNumber,
+    subjects: [subject1, subject2]
+};
+
+console.log(Student);
+
+
+
+
+
+
+
+// ask two numbers from user and sum then and console them
+let num1 = +prompt('enter first number');
+let num2 = +prompt('enter second number');
+
+// let sum = num1 + num2;
+console.log(num1 + num2);
+
+
+
+
+
+
+
+
+// ask user his 3 hobbies
+
+let hobi1 = prompt('Your 1st hobby');
+let hobi2 = prompt('Your 2nd hobby');
+let hobi3 = prompt('Your 3rd hobby');
+
+hobbies = [hobi1, hobi2, hobi3];
+console.log(hobbies);
+
+
+// Today lecture is complete
+
+
+
+
+
